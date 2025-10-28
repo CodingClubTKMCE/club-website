010d0c5fd02d386f00d599bcb322fc17a1a2ec2f
Swassyman <swassyman2005@gmail.com>
2025-10-28 16:49:25 +0530

    sveltekit init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import "../app.css";
+    import favicon from "$lib/assets/favicon.svg";
+    import logo from "$lib/assets/logo.png";
+    import { resolve } from "$app/paths";
+</script>
+
+<svelte:head>
+    <link rel="icon" href={favicon} />
+</svelte:head>
+
+<section class="min-h-screen flex flex-col bg-black">
+    <header class="bg-black">
+        <div class="container mx-auto p-6 flex justify-between items-center">
+            <a href={resolve("/")} title="logo">
+                <img class="w-[50px] h-[50px]" src={favicon} alt="club logo" />
+            </a>
+            <nav></nav>
+        </div>
+    </header>
+    <main class="flex-1 bg-white">
+        <slot />
+    </main>
+</section>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Welcome | CCTKM</title>
+</svelte:head>
+
+<section
+    class="flex flex-col items-center justify-center min-h-screen"
+></section>
