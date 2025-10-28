6055c267d51a70667aa66537c3a27b03417c1ade
Swassyman <swassyman2005@gmail.com>
2025-10-28 16:59:37 +0530

    chore: formatting and format features

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,8 @@
 </script>
 
 <svelte:head>
-    <title>Welcome | CCTKM</title>
+  <title>Welcome | CCTKM</title>
 </svelte:head>
 
-<section
-    class="flex flex-col items-center justify-center min-h-screen"
-></section>
+<section class="flex flex-col items-center justify-center min-h-screen">
+</section>
