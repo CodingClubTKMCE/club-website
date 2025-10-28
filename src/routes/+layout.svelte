be7cd07b87d32972be432b932d0c002cb70b359b
Swassyman <swassyman2005@gmail.com>
2025-10-28 17:59:16 +0530

    feat: header with no redirection

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 import "../app.css";
-import favicon from "$lib/assets/favicon.svg";
-// import logo from "$lib/assets/logo.png";
 import { resolve } from "$app/paths";
+import { page } from "$app/state";
+import favicon from "$lib/assets/favicon.svg";
+import logo from "$lib/assets/logo.png";
+import { Button } from "$lib/components/ui/button/index";
+import Mail from "@lucide/svelte/icons/mail";
 </script>
 
 <svelte:head>
@@ -11,14 +14,39 @@ import { resolve } from "$app/paths";
 
 <section class="min-h-screen flex flex-col bg-black">
   <header class="bg-black">
-    <div class="container mx-auto p-6 flex justify-between items-center">
+    <div class="container mx-auto flex flex-col-1 justify-between items-center">
       <a href={resolve("/")} title="logo">
-        <img class="w-[50px] h-[50px]" src={favicon} alt="club logo" />
+        <img class="w-auto h-auto" src={logo} alt="club logo" />
       </a>
-      <nav></nav>
+      <nav class="bg-black px-8 py-4">
+        <div class="flex items-center gap-4">
+          <a
+            href={resolve("/")}
+            class="{page.url.pathname === '/' ? 'rounded-full bg-neutral-800' : ''} px-8 py-3 text-white transition hover:bg-neutral-700"
+          >
+            Home
+          </a>
+          <a
+            href={resolve("/")}
+            class="{page.url.pathname.startsWith('/events') ? 'rounded-full bg-neutral-800' : ''} px-8 py-3 text-white transition hover:text-gray-300"
+          >
+            Events
+          </a>
+          <a
+            href={resolve("/")}
+            class="{page.url.pathname.startsWith('/execom') ? 'rounded-full bg-neutral-800' : ''} px-8 py-3 text-white transition hover:text-gray-300"
+          >
+            Execom
+          </a>
+        </div>
+      </nav>
+      <Button variant="outline" size="sm">
+        <Mail />
+        Login
+      </Button>
     </div>
   </header>
-  <main class="flex-1 bg-white">
+  <main class="flex-1 bg-gray-500">
     <slot />
   </main>
 </section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,3 @@
 <svelte:head>
   <title>Welcome | CCTKM</title>
 </svelte:head>
-
-<section class="flex flex-col items-center justify-center min-h-screen">
-</section>
